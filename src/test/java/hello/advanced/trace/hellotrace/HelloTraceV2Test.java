2a1c384f628354fedf3a72dde01dbccbd2e85025
h0l1da2 <holiday.k1@icloud.com>
2025-01-02 18:30:14 +0900

    test: Trace V2
    - test trace V2 that logger for synchronize
    
    holiday

--- src/test/java/hello/advanced/trace/hellotrace/HelloTraceV2Test.java
+++ src/test/java/hello/advanced/trace/hellotrace/HelloTraceV2Test.java
@@ -2,21 +2,28 @@ package hello.advanced.trace.hellotrace;
 
 import hello.advanced.app.trace.TraceStatus;
 import hello.advanced.app.trace.hellotrace.HelloTraceV1;
+import hello.advanced.app.trace.hellotrace.HelloTraceV2;
 import org.junit.jupiter.api.Test;
 
-public class HelloTraceV1Test {
+public class HelloTraceV2Test {
 
     @Test
     void begin_end() {
-        HelloTraceV1 trace = new HelloTraceV1();
-        TraceStatus status = trace.begin("hello");
-        trace.end(status);
+        HelloTraceV2 trace = new HelloTraceV2();
+        TraceStatus status1 = trace.begin("hello");
+        TraceStatus status2 = trace.beginSync(status1.getTraceId(), "hello2");
+
+        trace.end(status2);
+        trace.end(status1);
     }
 
     @Test
     void begin_exception() {
-        HelloTraceV1 trace = new HelloTraceV1();
-        TraceStatus status = trace.begin("hello");
-        trace.exception(status, new IllegalStateException());
+        HelloTraceV2 trace = new HelloTraceV2();
+        TraceStatus status1 = trace.begin("hello");
+        TraceStatus status2 = trace.beginSync(status1.getTraceId(), "hello2");
+
+        trace.exception(status2, new IllegalStateException());
+        trace.exception(status1, new IllegalStateException());
     }
 }
